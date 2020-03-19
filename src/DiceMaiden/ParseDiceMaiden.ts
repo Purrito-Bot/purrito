de7734e45437964b354c8e88465d5fa9e43ee0c9
Thomas Holmes <thomasholmes@outlook.com>
2020-03-19 22:49:06 +0000

    Dice result success made more accurate.

--- src/DiceMaiden/ParseDiceMaiden.ts
+++ src/DiceMaiden/ParseDiceMaiden.ts
@@ -7,34 +7,58 @@ import { DiceMaidenResult } from "./DiceMaidenResult";
 export function parseDiceMaidenMessage(messageContent: string): DiceMaidenResult {
 
     // example message:
-    // player_name Roll: `[5], [1]` Result: 6.
+    // player_name Roll: `[5], [1]` Result: 6
 
-    const result = new DiceMaidenResult();
+    const result = new DiceMaidenResult()
 
-    const rollerName = messageContent.match(/\S+/)
-    if (rollerName !== null) {
-        result.rollerName = rollerName[0];
+    const rollerName = extractRollerName(messageContent)
+    const totalResult = extractTotalResult(messageContent)
+    const diceRolls = extractDiceRolls(messageContent)
+
+    if (rollerName && totalResult && diceRolls) {
+        result.success = true
+        result.rollerName = rollerName
+        result.totalResult = totalResult
+        result.diceRolls = diceRolls
     }
+    return result
+}
 
+
+function extractDiceRolls(messageContent: string): number[] | null {
+    let result: number[] | null = null
+
+    const rollStrs = messageContent.match(/\[\d+\]/g)
+    if (rollStrs && rollStrs.length >= 1) {
+        const rolls = rollStrs
+            .map(rollStr => rollStr.replace('[', '').replace(']', ''))
+            .map(rollNumStr => Number.parseInt(rollNumStr, 10))
+
+        if (rolls.filter(roll => isNaN(roll)).length === 0) {
+
+            result = rolls
+        }
+    }
+    return result
+}
+
+function extractTotalResult(messageContent: string): number | null {
+    let result: number | null = null;
     const totalResultStr = messageContent.match(/(?<=Result:\s)\d+/)
     if (totalResultStr && totalResultStr.length === 1) {
         const totalResult = Number.parseInt(totalResultStr[0], 10)
         if (!isNaN(totalResult)) {
-            result.success = true
-            result.totalResult = totalResult
+            result = totalResult
         }
     }
+    return result
+}
 
-    const rollStrs = messageContent.match(/\[\d+\]/g)
-    if (rollStrs && rollStrs.length >= 1) {
-        rollStrs.forEach(rollStr => {
-            const rollNumStr = rollStr.replace('[', '').replace(']', '');
-            const rollResult = Number.parseInt(rollNumStr, 10)
-            if (!isNaN(rollResult)) {
-                result.diceRolls.push(rollResult)
-            }
-        })
+function extractRollerName(messageContent: string): string | null {
+    let result: string | null = null;
+    const rollerName = messageContent.match(/\S+/)
+    if (rollerName && rollerName.length === 1) {
+        result = rollerName[0]
     }
-
     return result
 }
