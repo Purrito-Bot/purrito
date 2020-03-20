bb8794a408615cdc7f68accf9d7b239772d391e0
Thomas Holmes <thomasholmes@outlook.com>
2020-03-20 14:53:03 +0000

    Added Jest and dice parser unit tests.

---
+++ src/__tests__/DiceMaiden/ParseDiceMaiden.test.ts
@@ -0,0 +1,52 @@
+import { parseDiceMaidenMessage } from '../../DiceMaiden/ParseDiceMaiden'
+
+test('Username is correctly parsed', () => {
+    
+    const message = 'example_player Roll: [20], [11] Result: 31'
+
+    const result = parseDiceMaidenMessage(message)
+
+    expect(result.parseSuccessful).toBeTruthy()
+    expect(result.rollerName).toStrictEqual('example_player')
+})
+
+test('Total result is correctly parsed', () => {
+    
+    const message = 'example_player Roll: [20], [11] Result: 31'
+
+    const result = parseDiceMaidenMessage(message)
+
+    expect(result.parseSuccessful).toBeTruthy()
+    expect(result.totalResult).toStrictEqual(31)
+})
+
+test('Dice rolls in different sets are correctly parsed', () => {
+    
+    const message = 'example_player Roll: [20], [11] Result: 31'
+
+    const result = parseDiceMaidenMessage(message)
+
+
+    expect(result.parseSuccessful).toBeTruthy()
+    expect(result.diceRolls).toStrictEqual([20, 11])
+})
+
+test('Dice rolls in the same sets are correctly parsed', () => {
+    
+    const message = 'example_player Roll: [20, 11] Result: 31'
+
+    const result = parseDiceMaidenMessage(message)
+
+    expect(result.parseSuccessful).toBeTruthy()
+    expect(result.diceRolls).toStrictEqual([20, 11])
+})
+
+test('Dice rolls in mixed sets are correctly parsed', () => {
+    
+    const message = 'example_player Roll: [20, 18], [11] Result: 31'
+
+    const result = parseDiceMaidenMessage(message)
+
+    expect(result.parseSuccessful).toBeTruthy()
+    expect(result.diceRolls).toStrictEqual([20, 18, 11])
+})
