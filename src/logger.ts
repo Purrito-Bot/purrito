a54cbd41e38193dce901bbed53ae34f4b6658b96
Thomas Holmes <thomasholmes@outlook.com>
2020-03-22 18:52:56 +0000

    Added timestamp and changed formatting of logs.

--- src/logger.ts
+++ src/logger.ts
@@ -2,10 +2,13 @@ import * as winston from 'winston';
 
 export const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }), winston.format.printf(info => `[${info.timestamp}] ${info.level}: ${info.message}`)),
   defaultMeta: { service: 'purrito' },
   transports: [
-    new winston.transports.File({ filename: 'purrito.log', level: 'info' }),
-    new winston.transports.Console({ format: winston.format.simple(), level: 'info' })
+    new winston.transports.File({ filename: 'purrito.log', level: 'debug', handleExceptions: true }),
+    new winston.transports.Console({ level: 'debug', handleExceptions: true })
   ]
 });
