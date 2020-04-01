8989520a0730679bc38c5d2cb2e78b17c019184b
Dan Austin <djadevelopment@gmail.com>
2020-04-01 10:15:27 +0100

    Add lazy conjugator for missed verbs

--- src/commands/do.ts
+++ src/commands/do.ts
@@ -6,14 +6,25 @@ export default async function _do(message: Message, args: string[]) {
     logger.debug('Executing command +do')
     const messageString = args.join(' ')
     const sentence = nlp(messageString)
-    const proceesedSentence = sentence
+    let proceesedSentence = sentence
         .verbs()
         .toPresentTense()
-        .trim()
-        .out('text')
+        .text()
     if (proceesedSentence.length < 1) {
-        logger.error('No verb detected in sentence: ' + messageString)
-        return await message.channel.send(`Purrito cannot ${args[0]}.`)
+        logger.error(
+            `No verb detected in sentence "${messageString}". Making best attempt.`
+        )
+        if (args.length > 1) {
+            proceesedSentence = `${lazyConjugate(args[0])} ${args
+                .slice(1)
+                .join(' ')}`
+        } else {
+            proceesedSentence = `${lazyConjugate(args[0])}`
+        }
     }
     return await message.channel.send(`*purrito ${proceesedSentence}*`)
+}
+
+function lazyConjugate(verb: string) {
+    return verb + 's'
 }
