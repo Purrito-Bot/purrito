8bb17b5aab6b64225293d1cb15951290ddf5a995
Matt Beech <matthew_beech@live.co.uk>
2020-10-09 16:52:49 +0100

    Separated generation logic into its own function

--- src/commands/generateEncounter.ts
+++ src/commands/generateEncounter.ts
@@ -7,12 +7,11 @@ import { calculateXpBudgetForDifficulty } from './encounter/xpUtils'
 
 /**
  * Given +generate encounter, generate an item based on the sets of data
- * @param message
- * @param args
+ * @param message the discord message
+ * @param args arguments provided for the command
  */
 export async function generateEncounter(message: Message, args?: string[]) {
     let messageToReturn: string
-
     // arg[1] - environment
     // arg[2] - difficulty
     if (Array.isArray(args) && args.length > 0) {
@@ -20,38 +19,9 @@ export async function generateEncounter(message: Message, args?: string[]) {
             messageToReturn =
                 'please provide details of your encounter e.g. +generate encounter mountain easy'
         } else {
-            const encounterEnvironment = args[1].toUpperCase() as Environment
+            const environment = args[1].toUpperCase() as Environment
             const difficulty = args[2].toUpperCase() as EncounterDifficulty
-
-            // Filter monsters based on the given environment
-            const potentialMonsters = filterMonstersByEnvironment(
-                encounterEnvironment
-            )
-
-            // Calculate the XP Budget
-            //TODO how can we sensibly pass in party details?
-            const xpBudget = calculateXpBudgetForDifficulty(
-                [7, 7, 2],
-                difficulty
-            )
-
-            let encounter = new Encounter()
-
-            while (encounter.totalXP < xpBudget) {
-                const monster = getRandomValueFromArray(potentialMonsters)
-
-                let encounterMonster = encounter.monsters.find(
-                    encounterMonster => encounterMonster.monster === monster
-                )
-
-                if (encounterMonster) {
-                    encounterMonster.amount = encounterMonster.amount + 1
-                } else {
-                    encounter.monsters.push({ monster, amount: 1 })
-                }
-
-                encounter.totalXP = encounter.totalXP + monster.xp
-            }
+            const encounter = _generateEncounter(environment, difficulty)
 
             messageToReturn = encounter.formatEncounterForMessage()
         }
@@ -61,4 +31,36 @@ export async function generateEncounter(message: Message, args?: string[]) {
     }
 
     await message.channel.send(messageToReturn)
+}
+
+function _generateEncounter(
+    environment: Environment,
+    difficulty: EncounterDifficulty
+) {
+    // Filter monsters based on the given environment
+    const potentialMonsters = filterMonstersByEnvironment(environment)
+
+    // Calculate the XP Budget
+    //TODO how can we sensibly pass in party details?
+    const xpBudget = calculateXpBudgetForDifficulty([7, 7, 2], difficulty)
+
+    let encounter = new Encounter()
+
+    while (encounter.totalXP < xpBudget) {
+        const monster = getRandomValueFromArray(potentialMonsters)
+
+        let encounterMonster = encounter.monsters.find(
+            encounterMonster => encounterMonster.monster === monster
+        )
+
+        if (encounterMonster) {
+            encounterMonster.amount = encounterMonster.amount + 1
+        } else {
+            encounter.monsters.push({ monster, amount: 1 })
+        }
+
+        encounter.totalXP = encounter.totalXP + monster.xp
+    }
+
+    return encounter
 }
