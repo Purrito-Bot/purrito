bd7813537ee99da4820660445f3edc38c9251068
Matt Beech <matthew_beech@live.co.uk>
2020-10-09 11:32:55 +0100

    Refactored how generation works, now there is a generic generate function, which works out what the user wants to generate, and executed the correct command

--- src/commands/generateItem.ts
+++ src/commands/generateItem.ts
@@ -1,16 +1,23 @@
 import { Message } from 'discord.js'
 import { Item } from '../models/item'
 
+/**
+ * Given +generate item, generate an item based on the sets of data
+ * @param message
+ * @param args
+ */
 export async function generateItem(message: Message, args?: string[]) {
     let messageToReturn: string
     const randomItem: Item = new Item()
 
-    let arg1: string | undefined
+    let outputVariant: string | undefined
+    // arg[0] we know will be "item" whereas arg[1] can be used to specify the
+    // variant. (lite or normal)
     if (Array.isArray(args) && args.length > 0) {
-        if (args[0].toLocaleLowerCase()) arg1 = args[0]
+        if (args[1]) outputVariant = args[1]
     }
 
-    switch (arg1) {
+    switch (outputVariant) {
         case 'lite':
             messageToReturn = randomItem.formateLiteMessage()
             break
