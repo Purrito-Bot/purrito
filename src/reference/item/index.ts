ea22eedfc21393b3f3d3a394dfab3f220a894032
Matt Beech <matthew_beech@live.co.uk>
2020-10-16 11:29:43 +0100

    Added new materials

--- src/reference/item/index.ts
+++ src/reference/item/index.ts
@@ -1,21 +1,30 @@
 import Ages from './ages.json'
 import Colours from './colours.json'
 import Descriptors from './descriptors.json'
+import Earthen from './earthen.json'
+import Fabrics from './fabrics.json'
+import Fantasy from './fantasy.json'
 import ItemConditions from './itemConditions.json'
 import { ItemTypes } from './itemTypes'
 import Metals from './metals.json'
 import Renowns from './renowns.json'
 import Sizes from './sizes.json'
+import Stones from './stones.json'
 import Woods from './woods.json'
 
 export {
-    Metals,
-    Woods,
     Ages,
     Colours,
+    Descriptors,
+    Earthen,
+    Fabrics,
+    Fantasy,
     ItemConditions,
     ItemTypes,
-    Descriptors,
+    Metals,
     Renowns,
     Sizes,
+    Stones,
+    Woods,
 }
+
--- src/reference/item/itemTypes.ts
+++ src/reference/item/itemTypes.ts
@@ -10,33 +10,33 @@ export const ItemTypes: ItemType[] = [
             },
         ],
     },
-    {
-        label: 'Magic Item - Table A',
-    },
-    {
-        label: 'Magic Item - Table B',
-    },
-    {
-        label: 'Magic Item - Table C',
-    },
-    {
-        label: 'Magic Item - Table D',
-    },
-    {
-        label: 'Magic Item - Table E',
-    },
-    {
-        label: 'Magic Item - Table F',
-    },
-    {
-        label: 'Magic Item - Table G',
-    },
-    {
-        label: 'Magic Item - Table H',
-    },
-    {
-        label: 'Magic Item - Table I',
-    },
+    // {
+    //     label: 'Magic Item - Table A',
+    // },
+    // {
+    //     label: 'Magic Item - Table B',
+    // },
+    // {
+    //     label: 'Magic Item - Table C',
+    // },
+    // {
+    //     label: 'Magic Item - Table D',
+    // },
+    // {
+    //     label: 'Magic Item - Table E',
+    // },
+    // {
+    //     label: 'Magic Item - Table F',
+    // },
+    // {
+    //     label: 'Magic Item - Table G',
+    // },
+    // {
+    //     label: 'Magic Item - Table H',
+    // },
+    // {
+    //     label: 'Magic Item - Table I',
+    // },
 ]
 
 export const idea: string[] = [
