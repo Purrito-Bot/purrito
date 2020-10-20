522ce5f42510517264c6c71832c632c9061dfbd6
Matt Beech <matthew_beech@live.co.uk>
2020-10-20 13:09:22 +0100

    Added thumbnails to song info embed

--- src/models/song.ts
+++ src/models/song.ts
@@ -6,17 +6,20 @@ export class Song implements PrintableObject {
     url: string
     description: string
     lengthInSeconds: string
+    thumbnailUrl?: string
 
     constructor(
         title: string,
         url: string,
         description: string,
-        lengthInSeconds: string
+        lengthInSeconds: string,
+        thumbnailUrl: string
     ) {
         this.title = title
         this.url = url
         this.description = description
         this.lengthInSeconds = lengthInSeconds
+        this.thumbnailUrl = thumbnailUrl
     }
     createEmbed(): MessageEmbed {
         const embed = new MessageEmbed()
@@ -24,6 +27,7 @@ export class Song implements PrintableObject {
         embed.setTitle(this.title)
         embed.setDescription(this.description)
         embed.setFooter(`Length: ${this.lengthInSeconds} seconds`)
+        if(this.thumbnailUrl) embed.setThumbnail(this.thumbnailUrl)
 
         return embed
     }
