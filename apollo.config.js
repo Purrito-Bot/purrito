aecde06afa4ce2b103edcd09851be36718f7f9dc
Matt Beech <matthew.beech@moonpig.com>
2021-07-02 11:13:31 +0100

    chore: convert apollo config to json format

--- apollo.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  client: {
-    service: 'bag-of-holding',
-    includes: ['./packages/**/*.{ts,tsx}'],
-    excludes: ['**/node_modules', '**/*.test.tsx', '**/__tests__/**'],
-    tagName: 'gql',
-  },
-};
